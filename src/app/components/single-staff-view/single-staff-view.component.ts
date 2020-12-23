17fdff7e7463291475f514e675ea9a42c7681896
Hamed Kshiem <keshim95@yahoo.com>
2020-12-23 03:57:53 +0100

    single staff view

--- src/app/components/single-staff-view/single-staff-view.component.ts
+++ src/app/components/single-staff-view/single-staff-view.component.ts
@@ -1,15 +1,26 @@
+import { Staff } from './../../Staff';
+import { StaffService } from './../../staff.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-staff-view',
   templateUrl: './single-staff-view.component.html',
-  styleUrls: ['./single-staff-view.component.scss']
+  styleUrls: ['./single-staff-view.component.scss'],
 })
 export class SingleStaffViewComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private route: ActivatedRoute, private staff: StaffService) {}
+  id: string;
+  staffObj: Staff;
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    console.log(this.id);
+    this.getStaffByID(this.id);
   }
-
+  getStaffByID(id) {
+    this.staff.getStaffById(id).subscribe((res) => {
+      this.staffObj = res;
+      console.log(res);
+    });
+  }
 }
