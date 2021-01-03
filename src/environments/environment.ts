364a6d88260734cf5e6c1411bf7eb3a83a38eabe
Hamed Kshiem <keshim95@yahoo.com>
2021-01-03 10:39:30 +0100

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API_URL: 'https://kvstaffapi.azurewebsites.net/api',
+  production: true,
+  API_URL: 'https://staffapise.azurewebsites.net/api',
 };
 
 /*
